d5f5cddc95793a59630e851bf7e8c7829ae84482
Volevach <v.volevach@gmail.com>
2016-01-06 23:20:08 +0100

    added wave proc sceletton

---
+++ FFT_Filter/FFT_Filter/Wave_Proc.cpp
@@ -0,0 +1,11 @@
+#include "Wave_Proc.h"
+
+
+Wave_Proc::Wave_Proc(void)
+{
+}
+
+
+Wave_Proc::~Wave_Proc(void)
+{
+}
---
+++ FFT_Filter/FFT_Filter/Wave_Proc.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "stdafx.h"
+
+class Wave_Proc
+{
+public:
+    Wave_Proc(void);
+    ~Wave_Proc(void);
+};
+
