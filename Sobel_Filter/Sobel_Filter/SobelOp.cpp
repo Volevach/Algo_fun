4ca092d37474476ca1bbecdf716082c9d79c82c3
Volevach <v.volevach@gmail.com>
2016-02-14 16:48:43 +0100

    commented sobelOp class

--- Sobel_Filter/Sobel_Filter/SobelOp.cpp
+++ Sobel_Filter/Sobel_Filter/SobelOp.cpp
@@ -1,58 +1,82 @@
+// main class for the Sobel implementation
+// 
+// inncludes a generic 2D convolution with a filter operator
+
 #include "SobelOp.h"
 
-
+// generic constructor
 SobelOp::SobelOp(void)
 {
 }
 
-
+// generic destructor
 SobelOp::~SobelOp(void)
 {
 }
 
-unsigned char SobelOp::SobelX(unsigned char *in, unsigned int xOffset)
+// perform Sobel in X direction on 2d array in p_in with row length u_xOffset
+unsigned char SobelOp::SobelX(unsigned char *p_in, unsigned int u_xOffset)
 {
     
-    short ret = 0;
-    ret -= ((short)(in[0]) + 2*(short)(in[xOffset + 0]) + (short)(in[2*xOffset + 0]));
-    ret += ((short)(in[2]) + 2*(short)(in[xOffset + 2]) + (short)(in[2*xOffset + 2]));
-    return (unsigned char)((ret>0)?ret:0);
+    short i_ret = 0;
+
+	// calculate the filtered pixel value
+    i_ret -= ((short)(p_in[0]) + 2*(short)(p_in[u_xOffset + 0]) + (short)(p_in[2*u_xOffset + 0]));
+    i_ret += ((short)(p_in[2]) + 2*(short)(p_in[u_xOffset + 2]) + (short)(p_in[2*u_xOffset + 2]));
+    
+	// return the value or 0 if value less than 0
+	return (unsigned char)((i_ret>0)?i_ret:0);
 }
 
-unsigned char SobelOp::SobelY(unsigned char *in, unsigned int xOffset)
+// perform Sobel in Y direction on 2d array in p_in with row length u_xOffset
+unsigned char SobelOp::SobelY(unsigned char *p_in, unsigned int u_xOffset)
 {
-    short ret = 0;
-    ret -= ((short)(in[0])         + 2*(short)(in[1]) +             (short)(in[2]));
-    ret += ((short)(in[2*xOffset]) + 2*(short)(in[2*xOffset + 1]) + (short)(in[2*xOffset + 2]));
-    return (unsigned char)((ret>0)?ret:0);
+    short i_ret = 0;
+
+	// calculate the filtered pixel value
+    i_ret -= ((short)(p_in[0])         + 2*(short)(p_in[1]) +             (short)(p_in[2]));
+    i_ret += ((short)(p_in[2*u_xOffset]) + 2*(short)(p_in[2*u_xOffset + 1]) + (short)(p_in[2*u_xOffset + 2]));
+    
+	// return the value or 0 if value less than 0
+	return (unsigned char)((i_ret>0)?i_ret:0);
 }
 
+// mormalize the x and y filtered pixel value to get a unidirectional sobel 
 unsigned char SobelOp::SobelNorm(unsigned char x, unsigned char y)
 {
+	// return sqrt(x^2 + y^2)
     return (unsigned char)(sqrt(((double)(x) * (double)(x)) + ((double)(y) * (double)(y))));
 }
 
-unsigned char SobelOp::SobelFilt(unsigned char *in, unsigned int xOffset)
+// filter input 2D pixel array with the sobel filter operator
+unsigned char SobelOp::SobelFilt(unsigned char *p_in, unsigned int u_xOffset)
 {
-    unsigned char ret = SobelNorm(SobelX(in, xOffset), SobelY(in, xOffset));
-    return (ret > 255) ? 255 : ret;
+	// calculate the unidirectional filtered pixel value
+    unsigned char u_ret = SobelNorm(SobelX(p_in, u_xOffset), SobelY(p_in, u_xOffset));
+    
+	// return vaslue truncsted to 8 bit
+	return (u_ret > 255) ? 255 : u_ret;
 }
 
-void SobelOp::SobelFiltImage(unsigned char in[MAX_X][MAX_Y], unsigned char out[MAX_X][MAX_Y])
+
+// filter input 2D pixel array with the sobel filter operator
+void SobelOp::SobelFiltImage(unsigned char p_in[MAX_X][MAX_Y], unsigned char p_out[MAX_X][MAX_Y])
 {
+	// run the sobel window for every pixel in the image of sixe MAX_X x MAX_Y
     for(unsigned int i = 0; i < MAX_X - 2; i++)
     {
         for(unsigned int j = 0; j < MAX_Y - 2; j++)
         {
-            out[i][j] = SobelFilt(&(in[i][j]), MAX_Y);
+            p_out[i][j] = SobelFilt(&(p_in[i][j]), MAX_Y);
         }
     }
 
+	// fill the corner pixels with zero
     for(unsigned int i = MAX_X - 2; i < MAX_X; i++)
     {
         for(unsigned int j = MAX_Y - 2; j < MAX_Y; j++)
         {
-            out[i][j] = 0;
+            p_out[i][j] = 0;
         }
     }
 }
--- Sobel_Filter/Sobel_Filter/SobelOp.h
+++ Sobel_Filter/Sobel_Filter/SobelOp.h
@@ -1,3 +1,7 @@
+// main class for the Sobel implementation
+// 
+// inncludes a generic 2D convolution with a filter operator
+
 #pragma once
 #include <math.h>
 #include "stdafx.h"
@@ -5,20 +9,34 @@
 class SobelOp
 {
 public:
+	// generic constructor
     SobelOp(void);
+
+	// generic destructor
     ~SobelOp(void);
 
-    void SobelFiltImage(unsigned char in[MAX_X][MAX_Y], unsigned char out[MAX_X][MAX_Y]);
+    // filter input 2D pixel array with the sobel filter operator
+	void SobelFiltImage(unsigned char p_in[MAX_X][MAX_Y], unsigned char p_out[MAX_X][MAX_Y]);
     
 
 private:
-    unsigned char SobelX(unsigned char *in, unsigned int xOffset);
-    unsigned char SobelY(unsigned char *in, unsigned int xOffset);
-    unsigned char SobelNorm(unsigned char x, unsigned char y);
-    unsigned char SobelFilt(unsigned char *in, unsigned int xOffset);
+	// perform Sobel in X direction on 2d array in p_in with row length u_xOffset
+    unsigned char SobelX(unsigned char *p_in, unsigned int u_xOffset);
+    
+	// perform Sobel in Y direction on 2d array in p_in with row length u_xOffset
+	unsigned char SobelY(unsigned char *p_in, unsigned int u_xOffset);
+    
+	// mormalize the x and y filtered pixel value to get a unidirectional sobel 
+	unsigned char SobelNorm(unsigned char x, unsigned char y);
+    
+	// main function to perform an unidirectional sobel on 2d array in p_in with row length u_xOffset
+	unsigned char SobelFilt(unsigned char *p_in, unsigned int u_xOffset);
 
 };
 
-const short sobelXMat[3][3] = {{-1,  0,  1}, {-2, 0 ,2}, {-1, 0, 1}};
-const short sobelYMat[3][3] = {{-1, -2, -1}, { 0, 0 ,0}, { 1, 2, 1}};
+// Sobel opersator for x direction
+const short c_sobelXMat[3][3] = {{-1,  0,  1}, {-2, 0 ,2}, {-1, 0, 1}};
+
+// Sobel opersator for x direction
+const short c_sobelYMat[3][3] = {{-1, -2, -1}, { 0, 0 ,0}, { 1, 2, 1}};
 
